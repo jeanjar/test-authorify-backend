6234ed1bd645088f0efd4966d1f4ff1dbab81cf4
Jean Jar Pereira de Araújo <jeanjpa@gmail.com>
2021-12-23 08:14:15 -0300

    refactoring

--- src/Controllers/CalendarController.js
+++ src/Controllers/CalendarController.js
@@ -2,8 +2,67 @@ const DayModeException = require('../utils/exceptions/DayModeException')
 const {MonthRangeValidator} = require('../validator')
 const dayjs = require("dayjs");
 
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const daysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+
+function buildDates(year, month, action = 'current') {
+    const date = dayjs().set('year', year).set('month', month).startOf('month');
+
+    if (action === 'current') {
+        return {
+            month: date.month(),
+            monthName: date.format('MMMM'),
+            year: date.year(),
+        }
+    }
+
+    const mapActions = {
+        next: 'add',
+        previous: 'subtract',
+    };
+
+    return {
+        month: date[mapActions[action]](1, 'month').month(),
+        monthName: date[mapActions[action]](1, 'month').format('MMMM'),
+        year: date[mapActions[action]](1, 'month').year(),
+    }
+}
+
+function buildCalendar(year, month) {
+
+    const firstDay = dayjs().set('year', year).set('month', month).set('date', 0)
+    const daysInMonth = dayjs().set('year', year).set('month', month + 1).set('date', 0).date()
+    const dayOfWeek = firstDay.day()
+    const today = parseInt(dayjs().set('year', year).set('month', month).format('DD'))
+
+    let calendar = []
+
+    for (let i = 0; i <= dayOfWeek; i++) {
+        calendar.push({
+            currentMonth: false,
+            currentDay: false,
+            text: '-',
+        })
+    }
+
+    for (let i = 0; i < daysInMonth; i++) {
+        const next = i + 1
+        calendar.push({
+            currentMonth: true,
+            currentDay: next === today,
+            text: next.toString(),
+        })
+    }
+
+    while (calendar.length % 7 !== 0) {
+        calendar.push({
+            currentMonth: false,
+            currentDay: false,
+            text: '-',
+        })
+    }
+
+    return calendar
+}
 
 module.exports = {
 
@@ -12,47 +71,13 @@ module.exports = {
         curMonth = curMonth ? parseInt(curMonth) : dayjs().month()
         curYear = curYear ? parseInt(curYear) : dayjs().year()
 
-        const baseDate = dayjs().set('year', curYear).set('month', curMonth).startOf('month');
-
-        const current = {
-            month: baseDate.month(),
-            monthName: baseDate.format('MMMM'),
-            year: baseDate.year(),
-        }
-
-        const next = {
-            month: baseDate.add(1, 'month').month(),
-            monthName: baseDate.add(1, 'month').format('MMMM'),
-            year: baseDate.add(1, 'month').year(),
-        }
-
-        const previous = {
-            month: baseDate.add(-1, 'month').month(),
-            monthName: baseDate.add(-1, 'month').format('MMMM'),
-            year: baseDate.add(-1, 'month').year(),
-        }
-
         MonthRangeValidator(curMonth)
 
-        const curMonthName = months[curMonth]
-        const firstDay = new Date(curYear, curMonth, 0)
-        const daysInMonth = new Date(curYear, curMonth + 1, 0).getDate()
-        const dayOfWeek = firstDay.getDay()
+        const current = buildDates(curYear, curMonth)
+        const next = buildDates(curYear, curMonth, 'next')
+        const previous = buildDates(curYear, curMonth, 'previous')
+        const calendar = buildCalendar(curYear, curMonth)
 
-        let calendar = [];
-
-        for (let i = 0; i <= dayOfWeek; i++) {
-            calendar.push('-');
-        }
-
-        for (let i = 0; i < daysInMonth; i++) {
-            const next = i + 1;
-            calendar.push(next);
-        }
-
-        while (calendar.length % 7 !== 0) {
-            calendar.push('-')
-        }
 
         return response.json({daysOfWeek, calendar, current, next, previous})
     }
